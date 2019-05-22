b9a56c3a5e2b179ca9ac4eb71652ea3220eb9b8d
Myron <dubtwee@gmail.com>
2019-05-22 11:55:12 -0500

    add import of elementListReducer

--- src/modules/redux/reducers/all.reducers.js
+++ src/modules/redux/reducers/all.reducers.js
@@ -1,7 +1,9 @@
 import firstReducer from './first.reducer';
 import secondReducer from './second.reducer';
+import elementListReducer from './elementList.reducer';
 
 export {
     firstReducer,
     secondReducer,
+    elementListReducer
 };
